32cac4e7c476c5d025a2bb33c1ea1cad6aa13902
Valerie Remaker <44219004+vremaker@users.noreply.github.com>
2020-12-15 19:10:45 -0800

    progress

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import get_data
 import logging
 
 app = Flask(__name__)
+
+current = "val"
+
 @app.route("/")
 def main_handler():
     app.logger.info("In MainHandler")
@@ -20,11 +23,15 @@ def greet_response_handler():
     app.logger.info(greet_types)
     #  print(test.__class__.__name__)
     yee =get_data.curate_feed(greet_types)
-    return render_template('feed.html',page_title="mEmEs",
-    greetings=[add_to_card(t) for t in yee])
+    return render_template('feed.html',page_title="Your Personalized Feed",
+    greetings=[add_to_card(t) for t in yee], user=current)
 
+@app.route("/login")
+def login():
+    return "HI"
 if __name__ == "__main__":
     # Used when running locally only.
     # When deploying to Google AppEngine, a webserver process will
     # serve your app.
     app.run(host="localhost", port=5000, debug=True)
+
